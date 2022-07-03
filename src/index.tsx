c2ac82171048bd737044eec44add917c4e03f7e2
罗畅1810 <chang.luo@xtransfer.cn>
2022-07-03 22:41:28 +0800

    master3

--- src/index.tsx
+++ src/index.tsx
@@ -30,6 +30,8 @@ const func1 = (str: string): void => {
 func1("我现在在使用Babel!哈哈");
 const num1 = 'a'
 const num2 = 'b'
+console.log('master');
+
 ReactDom.render(
     <div className="box">Hello React!===========</div>,
     document.getElementById("root")
